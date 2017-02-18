fdbd505753b8f5ebc6778a4bf2c1c10b2867dbbd
philipwhen <wzzh1203@gmail.com>
2017-02-18 15:02:56 -0500

    fix main

--- testmain.cpp
+++ testmain.cpp
@@ -14,6 +14,11 @@
 #include <unistd.h>
 #include <vector>
 
+void execute(Kmeans *algorithm, Dataset const *x, unsigned short k, unsigned short const *assignment,
+    int numthreads,
+    int maxIterations
+    );
+
 int main(int argc, char **argv){
     Dataset *x = NULL;
     unsigned short *assignment = NULL;
@@ -22,8 +27,8 @@ int main(int argc, char **argv){
     Kmeans *algorithm = NULL;
 
     int numthread = 1;
-    int maxIterations = std::numeric_limits<int>::max();
-
+//    int maxIterations = std::numeric_limits<int>::max();
+    int maxIterations = 500;
     std::string dataFilename;
     dataFilename = "";
     std::ifstream input(dataFilename.c_str());
@@ -43,7 +48,7 @@ int main(int argc, char **argv){
     k = 10;
     std::string method = "kmeansplusplus";
     Dataset *c = NULL;
-    c = init_centers_kmeanspp_v2(*x, k)
+    c = init_centers(*x, k);
 
     delete [] assignment;
     assignment = new unsigned short[x->n];
@@ -59,6 +64,53 @@ int main(int argc, char **argv){
     algorithm = NULL;
 }
 
+rusage get_time() {
+    rusage now;
+    getrusage(RUSAGE_SELF, &now);
+    return now;
+}
+
+
+double get_wall_time(){
+    struct timeval time;
+    if (gettimeofday(&time,NULL)){
+        //  Handle error
+        return 0;
+    }
+    return (double)time.tv_sec + (double)time.tv_usec * .000001;
+}
+
+int timeval_subtract(timeval *result, timeval *x, timeval *y) {
+    /* Perform the carry for the later subtraction by updating y. */
+    if (x->tv_usec < y->tv_usec) {
+        int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+        y->tv_usec -= 1000000 * nsec;
+        y->tv_sec += nsec;
+    }
+    if (x->tv_usec - y->tv_usec > 1000000) {
+        int nsec = (x->tv_usec - y->tv_usec) / 1000000;
+        y->tv_usec += 1000000 * nsec;
+        y->tv_sec -= nsec;
+    }
+
+    /* Compute the time remaining to wait.  tv_usec is certainly positive. */
+    result->tv_sec = x->tv_sec - y->tv_sec;
+    result->tv_usec = x->tv_usec - y->tv_usec;
+
+    /* Return 1 if result
+     * is negative. */
+    return x->tv_sec < y->tv_sec;
+}
+
+double elapsed_time(rusage *start) {
+    rusage now;
+    timeval diff;
+    getrusage(RUSAGE_SELF, &now);
+    timeval_subtract(&diff, &now.ru_utime, &start->ru_utime);
+
+    return (double)diff.tv_sec + (double)diff.tv_usec / 1e6;
+}
+
 void execute(Kmeans *algorithm, Dataset const *x, unsigned short k, unsigned short const *assignment,
     int numthreads,
     int maxIterations
@@ -71,7 +123,7 @@ void execute(Kmeans *algorithm, Dataset const *x, unsigned short k, unsigned sho
     double start_wall_time = get_wall_time();
     algorithm->initialize(x,k,workingassignment, numthreads);
     int iterations = algorithm->run(maxIterations);
-    double cluster_time = elapsed_time(&start_clustering_time);
+    double cluster_time = elapsed_time(&start_time);
     double cluster_wall_time = get_wall_time() - start_wall_time;
     std::cout << std::setw(5) << iterations << "\t";
     std::cout << std::setw(10) << numthreads << "\t";
