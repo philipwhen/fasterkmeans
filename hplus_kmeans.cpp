e432fa3e69c325c3309e53f49004a58ad0bb1bd4
philipwhen <wzzh1203@gmail.com>
2017-03-05 09:41:40 -0500

    add new file

---
+++ hplus_kmeans.cpp
@@ -0,0 +1,98 @@
+#include "hplus_kmeans.h"
+#include "general_functions.h"
+#include <cmath>
+
+//int HplusKmeans::runThread(int x, int y){ std::cout << "hello"; return 0;}
+int HplusKmeans::runThread(int threadId, int maxIterations){
+    int iterations = 0;
+
+    int startNdx = start(threadId);
+    int endNdx = end(threadId);
+
+    while ((iterations < maxIterations) && ! converged){
+        ++iterations;
+
+        update_s(threadId);
+        synchronizeAllThreads();
+
+        for (int i = startNdx; i < endNdx; ++i){
+            unsigned short closest = assignment[i];
+
+            double upper_comparison_bound = std::max(s[closest], lower[i]);
+
+            if (upper[i] <= upper_comparison_bound){
+                continue;
+            }
+            double u2 = pointCenterDist2(i, closest);
+            upper[i] = sqrt(u2);
+
+            if (upper[i] <= upper_comparison_bound){
+            continue;
+            }
+
+            double l2 = std::numeric_limits<double>::max();
+            for (int j = 0; j < k; j++){
+                if (j == closest){continue;}
+                double dist2 = pointCenterDist2(i,j);
+
+                if (dist2 < u2){
+                    l2 = u2;
+                    u2 = dist2;
+                    closest = j;
+                }else if (dist2 < l2){
+                    l2 = dist2;
+                }
+
+            }
+            lower[i] = sqrt(l2);
+
+            if (assignment[i] != closest){
+                upper[i] = sqrt(u2);
+                secondclosest[i] = assignment[i];
+                changeAssignment(i, closest, threadId);
+            }
+        }
+        verifyAssignment(iterations, startNdx, endNdx);
+
+        synchronizeAllThreads();
+        if (threadId == 0){
+            int furthestMovingCenter = move_centers();
+            converged = (0.0 == centerMovement[furthestMovingCenter]);
+        }
+
+        synchronizeAllThreads();
+
+        if (! converged) {
+            update_bounds(startNdx, endNdx);
+        }
+        synchronizeAllThreads();
+
+    }
+    return iterations;
+
+}
+
+void HplusKmeans::update_bounds(int startNdx, int endNdx){
+    int furthestMovingCenter = 0;
+    double longest = centerMovement[furthestMovingCenter];
+    double secondLongest = 0.0;
+    for (int j = 0; j < k; ++j){
+        if (longest < centerMovement[j]){
+            secondLongest = longest;
+            longest = centerMovement[j];
+        }else if (secondLongest < centerMovement[j]){
+            secondLongest = centerMovement[j];
+        }
+    }
+
+    for (int i = startNdx; i < endNdx; ++i){
+//        upper[i] += centerMovement[assignment[i]];
+        double sum = 0.0;
+        for (int dim = 0; dim < d; dim++){
+            sum += (*(x->data + i*d + dim) -(*centers)(assignment[i], dim)) * (*cmv)(assignment[i], dim);
+        }
+        if (sum < 0.0){upper[i] += centerMovement[assignment[i]];}
+        lower[i] -= (assignment[i] == furthestMovingCenter) ? secondLongest : longest;
+
+    }
+}
